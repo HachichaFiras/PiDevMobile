0457824c534c62db7101eb6642ee66a7f03d10c8
maram haded <haded.maram@esprit.tn>
2023-05-10 10:54:55 +0100

    rapport

---
+++ src/com/ipmobile/gui/GestionRapport.java
@@ -0,0 +1,189 @@
+package com.ipmobile.gui;
+
+
+import com.codename1.ui.*;
+import com.codename1.ui.layouts.BoxLayout;
+import com.ipmobile.entities.Rapport;
+import com.ipmobile.services.RapportService;
+import com.ipmobile.utils.AlertUtils;
+
+public class GestionRapport extends Form {
+
+
+    Rapport currentRapport;
+
+    TextField titreTF;
+    TextField descriptionTF;
+    TextField etatTF;
+
+    
+
+    TextField emailTF;
+    Label titreLabel;
+    Label etatLabel;
+
+    Label emailLabel;
+    Label descriptionLabel;
+    PickerComponent dateTF;
+
+    Button manageButton;
+
+    Form previous;
+
+    public GestionRapport(Form previous) {
+        super(ShowAll.currentRapport == null ? "Ajouter" : "Modifier", new BoxLayout(BoxLayout.Y_AXIS));
+        this.previous = previous;
+
+        currentRapport = ShowAll.currentRapport;
+
+        addGUIs();
+        addActions();
+
+        getToolbar().addMaterialCommandToLeftBar("  ", FontImage.MATERIAL_ARROW_BACK, e -> previous.showBack());
+    }
+
+    private void addGUIs() {
+
+
+        dateTF = PickerComponent.createDate(null).label("Date");
+
+
+        titreLabel = new Label("Titre : ");
+        titreLabel.setUIID("labelDefault");
+        titreTF = new TextField();
+        titreTF.setHint("Tapez le titre");
+
+
+        etatLabel = new Label("Etat : ");
+        etatLabel.setUIID("labelDefault");
+        etatTF = new TextField();
+        etatTF.setHint("Tapez le etat");
+
+        descriptionLabel = new Label("Description : ");
+        descriptionLabel.setUIID("labelDefault");
+        descriptionTF = new TextField();
+        descriptionTF.setHint("Tapez le description");
+
+        descriptionLabel = new Label("Description : ");
+        descriptionLabel.setUIID("labelDefault");
+        descriptionTF = new TextField();
+        descriptionTF.setHint("Tapez le description");
+
+        emailLabel = new Label("Email : ");
+        emailLabel.setUIID("labelDefault");
+        emailTF = new TextField();
+        emailTF.setHint("Tapez le description");
+
+
+        if (currentRapport == null) {
+
+
+            manageButton = new Button("Ajouter");
+        } else {
+            dateTF.getPicker().setDate(currentRapport.getDateCreation());
+            titreTF.setText(currentRapport.getTitre());
+            descriptionTF.setText(currentRapport.getDescription());
+            etatTF.setText(currentRapport.getEtat());
+
+
+
+            manageButton = new Button("Modifier");
+        }
+        manageButton.setUIID("buttonWhiteCenter");
+
+        Container container = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+        container.setUIID("containerRounded");
+
+        container.addAll(
+                dateTF,
+                titreLabel, titreTF,
+                etatLabel, etatTF,
+                descriptionLabel, descriptionTF,
+                emailLabel, emailTF,
+                manageButton
+        );
+
+        this.addAll(container);
+    }
+
+    private void addActions() {
+
+        if (currentRapport == null) {
+            manageButton.addActionListener(action -> {
+                if (controleDeSaisie()) {
+                    int responseCode = RapportService.getInstance().add(
+                            new Rapport(
+                                    dateTF.getPicker().getDate(),
+                                    titreTF.getText(),
+                                    etatTF.getText(),
+                                    descriptionTF.getText()
+                            ),
+                            emailTF.getText()
+                    );
+                    if (responseCode == 200) {
+                        AlertUtils.makeNotification("ajout ajouté avec succes");
+                        showBackAndRefresh();
+                    } else {
+                        Dialog.show("Erreur", "Erreur d'ajout de rapport. Code d'erreur : " + responseCode, new Command("Ok"));
+                    }
+                }
+            });
+        } else {
+            manageButton.addActionListener(action -> {
+                if (controleDeSaisie()) {
+                    int responseCode = RapportService.getInstance().edit(
+                            new Rapport(
+                                    currentRapport.getId(),
+                                    dateTF.getPicker().getDate(),
+                                    titreTF.getText(),
+                                    etatTF.getText(),
+                                    descriptionTF.getText()
+
+                            )
+                    );
+                    if (responseCode == 200) {
+                        AlertUtils.makeNotification("Rapport modifié avec succes");
+                        showBackAndRefresh();
+                    } else {
+                        Dialog.show("Erreur", "Erreur de modification de rapport. Code d'erreur : " + responseCode, new Command("Ok"));
+                    }
+                }
+            });
+        }
+    }
+
+    private void showBackAndRefresh() {
+        ((ShowAll) previous).refresh();
+        previous.showBack();
+    }
+
+    private boolean controleDeSaisie() {
+
+
+        if (dateTF.getPicker().getDate() == null) {
+            Dialog.show("Avertissement", "Veuillez saisir la date", new Command("Ok"));
+            return false;
+        }
+
+
+        if (titreTF.getText().equals("")) {
+            Dialog.show("Avertissement", "Titre vide", new Command("Ok"));
+            return false;
+        }
+
+
+        if (etatTF.getText().equals("")) {
+            Dialog.show("Avertissement", "Etat vide", new Command("Ok"));
+            return false;
+        }
+
+
+        if (descriptionTF.getText().equals("")) {
+            Dialog.show("Avertissement", "Description vide", new Command("Ok"));
+            return false;
+        }
+
+
+        return true;
+    }
+}
---
+++ src/com/ipmobile/gui/ShowAll.java
@@ -0,0 +1,173 @@
+package com.ipmobile.gui;
+
+import com.codename1.components.InteractionDialog;
+import com.codename1.ui.*;
+import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.layouts.BoxLayout;
+import com.ipmobile.entities.Rapport;
+import com.ipmobile.services.RapportService;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public class ShowAll extends Form {
+
+    Form previous;
+
+    public static Rapport currentRapport = null;
+    Button addBtn;
+
+    TextField searchTF;
+    ArrayList<Component> componentModels;
+
+
+    public ShowAll(Form previous) {
+        super("Rapport", new BoxLayout(BoxLayout.Y_AXIS));
+        this.previous = previous;
+
+        addGUIs();
+        addActions();
+
+        super.getToolbar().addMaterialCommandToLeftBar("  ", FontImage.MATERIAL_ARROW_BACK, e -> previous.showBack());
+    }
+
+    public void refresh() {
+        this.removeAll();
+        addGUIs();
+        addActions();
+        this.refreshTheme();
+    }
+
+    private void addGUIs() {
+        addBtn = new Button("Ajouter");
+        addBtn.setUIID("buttonWhiteCenter");
+        this.add(addBtn);
+
+
+        ArrayList<Rapport> listRapport = RapportService.getInstance().getAll();
+        componentModels = new ArrayList<>();
+
+        searchTF = new TextField("", "Chercher rapport par Type");
+        searchTF.addDataChangedListener((d, t) -> {
+            if (componentModels.size() > 0) {
+                for (Component componentModel : componentModels) {
+                    this.removeComponent(componentModel);
+                }
+            }
+            componentModels = new ArrayList<>();
+            for (Rapport rapport : listRapport) {
+                if (rapport.getTitre().toLowerCase().startsWith(searchTF.getText().toLowerCase())) {
+                    Component model = makeRapportModel(rapport);
+                    this.add(model);
+                    componentModels.add(model);
+                }
+            }
+            this.revalidate();
+        });
+        this.add(searchTF);
+
+
+        if (listRapport.size() > 0) {
+            for (Rapport rapport : listRapport) {
+                Component model = makeRapportModel(rapport);
+                this.add(model);
+                componentModels.add(model);
+            }
+        } else {
+            this.add(new Label("Aucune donnée"));
+        }
+    }
+
+    private void addActions() {
+        addBtn.addActionListener(action -> {
+            currentRapport = null;
+            new GestionRapport(this).show();
+        });
+
+    }
+
+    Label dateLabel, titreLabel, etatLabel, descriptionLabel ;
+
+
+    private Container makeModelWithoutButtons(Rapport rapport) {
+        Container reclamationModel = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+        reclamationModel.setUIID("containerRounded");
+
+
+        dateLabel = new Label("Date : " + new SimpleDateFormat("dd-MM-yyyy").format(rapport.getDateCreation()));
+        dateLabel.setUIID("labelDefault");
+
+        titreLabel = new Label("Titre : " + rapport.getTitre());
+        titreLabel.setUIID("labelDefault");
+        
+          descriptionLabel = new Label("Description : " + rapport.getDescription());
+        descriptionLabel.setUIID("labelDefault");
+
+        etatLabel = new Label("Etat : " + rapport.getEtat());
+        etatLabel.setUIID("labelDefault");
+
+      
+
+
+        reclamationModel.addAll(
+
+                dateLabel, titreLabel, etatLabel, descriptionLabel
+        );
+
+        return reclamationModel;
+    }
+
+    Button editBtn, deleteBtn;
+    Container btnsContainer;
+
+    private Component makeRapportModel(Rapport rapport) {
+
+        Container rapportModel = makeModelWithoutButtons(rapport);
+
+        btnsContainer = new Container(new BorderLayout());
+        btnsContainer.setUIID("containerButtons");
+
+        editBtn = new Button("Modifier");
+        editBtn.setUIID("buttonWhiteCenter");
+        editBtn.addActionListener(action -> {
+            currentRapport = rapport;
+            new GestionRapport(this).show();
+        });
+
+        deleteBtn = new Button("Supprimer");
+        deleteBtn.setUIID("buttonWhiteCenter");
+        deleteBtn.addActionListener(action -> {
+            InteractionDialog dlg = new InteractionDialog("Confirmer la suppression");
+            dlg.setLayout(new BorderLayout());
+            dlg.add(BorderLayout.CENTER, new Label("Voulez vous vraiment supprimer ce reclamation ?"));
+            Button btnClose = new Button("Annuler");
+            btnClose.addActionListener((ee) -> dlg.dispose());
+            Button btnConfirm = new Button("Confirmer");
+            btnConfirm.addActionListener(actionConf -> {
+                int responseCode = RapportService.getInstance().delete(rapport.getId());
+
+                if (responseCode == 200) {
+                    currentRapport = null;
+                    dlg.dispose();
+                    rapportModel.remove();
+                    this.refreshTheme();
+                } else {
+                    Dialog.show("Erreur", "Erreur de suppression du rapport. Code d'erreur : " + responseCode, new Command("Ok"));
+                }
+            });
+            Container btnContainer = new Container(new BoxLayout(BoxLayout.X_AXIS));
+            btnContainer.addAll(btnClose, btnConfirm);
+            dlg.addComponent(BorderLayout.SOUTH, btnContainer);
+            dlg.show(800, 800, 0, 0);
+        });
+
+        btnsContainer.add(BorderLayout.WEST, editBtn);
+        btnsContainer.add(BorderLayout.EAST, deleteBtn);
+
+
+        rapportModel.add(btnsContainer);
+
+        return rapportModel;
+    }
+
+}
